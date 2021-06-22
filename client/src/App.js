a587537d155d7484250c5ce2c3eae952fa3ade18
Vivek <vukondpalle@gmail.com>
2021-06-22 22:52:49 +0530

    inital commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <input type="text" />
+      <div>
+        hello
+      </div>
+      <div>
+        how r u
+      </div>
+      <div>
+        this is changed again
+      </div>
+    </div>
+  );
+}
+
+export default App;
